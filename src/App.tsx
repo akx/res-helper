042512f6489af51eabb16cbbea93a68766c4883a
Aarni Koskela <akx@iki.fi>
2023-12-12 12:04:50 +0200

    Add presets

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,21 @@ export default function App() {
   const [minSize, setMinSize] = useState<number>(512);
   const [maxSize, setMaxSize] = useState<number>(2048);
   const [quantization, setQuantization] = useState<number>(64);
+
+  const setMidSize = (size: number) => {
+    setTargetMpix((size * size) / 1024 / 1024);
+    setMinSize(size / 2);
+    setMaxSize(size * 2);
+  };
+
+  const loadSD15Preset = () => {
+    setMidSize(512);
+  };
+
+  const loadSDXLPreset = () => {
+    setMidSize(1024);
+  };
+
   const resolutions = useDebouncedMemo(
     () =>
       calculateResolutions({
@@ -28,58 +43,70 @@ export default function App() {
     [targetMpix, pixLeeway, minAR, maxAR, minSize, maxSize, quantization],
     200,
   );
-
   return (
-    <div id="container">
+    <>
       <aside>
-        <NumberControl
-          label="Target Megapixels"
-          value={targetMpix}
-          min={0.01}
-          step={0.5}
-          sliderMax={5}
-          onChange={setTargetMpix}
-        />
-        <NumberControl
-          label="Pix Leeway"
-          value={pixLeeway}
-          min={0}
-          max={1}
-          step={0.01}
-          onChange={setPixLeeway}
-        />
-        <NumberControl
-          label="Min AR"
-          min={0}
-          step={0.01}
-          sliderMax={4}
-          value={minAR}
-          onChange={setMinAR}
-        />
-        <NumberControl
-          label="Max AR"
-          min={0}
-          step={0.01}
-          sliderMax={4}
-          value={maxAR}
-          onChange={setMaxAR}
-        />
-        <NumberControl
-          label="Min Size"
-          min={0}
-          step={8}
-          sliderMax={4096}
-          value={minSize}
-          onChange={setMinSize}
-        />
-        <NumberControl
-          label="Max Size"
-          min={0}
-          step={8}
-          sliderMax={4096}
-          value={maxSize}
-          onChange={setMaxSize}
-        />
+        <fieldset>
+          <legend>Presets</legend>
+          <div className="button-group">
+            <button onClick={loadSD15Preset}>SD1.5 (512&times;512)</button>
+            <button onClick={loadSDXLPreset}>SDXL (1024&times;1024)</button>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>Target Size</legend>
+          <NumberControl
+            label="Target Megapixels"
+            value={targetMpix}
+            min={0.01}
+            step={0.5}
+            sliderMax={5}
+            onChange={setTargetMpix}
+          />
+          <NumberControl
+            label="Allowed Target Leeway"
+            value={pixLeeway}
+            min={0}
+            max={1}
+            step={0.01}
+            onChange={setPixLeeway}
+          />
+          <NumberControl
+            label="Minimum Size"
+            min={0}
+            step={8}
+            sliderMax={4096}
+            value={minSize}
+            onChange={setMinSize}
+          />
+          <NumberControl
+            label="Maximum Size"
+            min={0}
+            step={8}
+            sliderMax={4096}
+            value={maxSize}
+            onChange={setMaxSize}
+          />
+        </fieldset>
+        <fieldset>
+          <legend>Aspect Ratio</legend>
+          <NumberControl
+            label="Minimum AR"
+            min={0}
+            step={0.01}
+            sliderMax={4}
+            value={minAR}
+            onChange={setMinAR}
+          />
+          <NumberControl
+            label="Maximum AR"
+            min={0}
+            step={0.01}
+            sliderMax={4}
+            value={maxAR}
+            onChange={setMaxAR}
+          />
+        </fieldset>
         <NumberControl
           label="Quantization"
           min={8}
@@ -95,6 +122,6 @@ export default function App() {
           pixLeeway={pixLeeway}
         />
       </main>
-    </div>
+    </>
   );
 }
